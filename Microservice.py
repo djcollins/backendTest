a3f9769071e486c75136ddf7d0454af9709f8c3a
Daniel Collins <daniel@v-f-g.com>
2020-11-09 08:49:59 +0200

    working python script allowing for user to enter a string enclosed byte array

--- Microservice.py
+++ Microservice.py
@@ -1,22 +1,28 @@
 from nameko.rpc import rpc
 import dahuffman
 codec=dahuffman.load_shakespeare()
-
+encoded_strings={} #the dictionary of string: compressed string
+encoded_string_to_string={} #dictionary of compressed string bytes as a string : the bytes
 class Microservice:
+    
     name = "micro_service"
     @rpc
-    def hello(self, name):
+    def hello(self, name):#From a tutorial. thought I'd leave it here
         return "Hello, {}!".format(name)
     @rpc
     def encode_strings(self,str_list):
-        string_encoded={}
-        for string in str_list: 
-            string_encoded[s]=codec.encode(s)
-        return string_encoded[s]
+
+        for string in str_list:
+            #adding the encoded string to a dictionary, the key is the string, the value is the compressed string
+            encoded_strings[string]=codec.encode(string)
+            encoded_string_to_string[str(encoded_strings[string])]=encoded_strings[string]
+        return str(encoded_strings)
     @rpc
     def decode_string(self,s):
-        return codec.decode(s)
+        return str(codec.decode(encoded_string_to_string[s]))
     @rpc
     def square_odd(self, num_list):
-        return [x**2 for x in num_list]
-    #encode_string_list =lambda xs: {x:this.encode_string(x) for x in xs}
+        return [x*x if x%2==1 else x for x in num_list]
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # backendTest
 Backend Engineering test - Daniel Collins
+Allowing the user to input bytes without errors being thrown left right and center requried me to make a dictionary where the string value of the bytes mapped to the byte value of bytes? Does this make sense?

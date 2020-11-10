09000605f0b2ac4b098eb98987cab0bfc0945725
Daniel John Collins <danielcollins96@gmail.com>
2020-11-10 21:13:54 +0200

    Update Microservice.py
    final edit to python script

--- Microservice.py
+++ Microservice.py
@@ -1,14 +1,12 @@
+#Daniel Collins 
 from nameko.rpc import rpc
-import dahuffman
-codec=dahuffman.load_shakespeare()
+from dahuffman import load_shakespeare
+codec=load_shakespeare()
 encoded_strings={} #the dictionary of string: compressed string
 encoded_string_to_string={} #dictionary of compressed string bytes as a string : the bytes
 class Microservice:
-    
+    #Standard python microservice class
     name = "micro_service"
-    @rpc
-    def hello(self, name):#From a tutorial. thought I'd leave it here
-        return "Hello, {}!".format(name)
     @rpc
     def encode_strings(self,str_list):
 
@@ -18,11 +16,18 @@ class Microservice:
             encoded_string_to_string[str(encoded_strings[string])]=encoded_strings[string]
         return str(encoded_strings)
     @rpc
-    def decode_string(self,s):
-        return str(codec.decode(encoded_string_to_string[s]))
+    def decode_string(self,string):
+        return str(codec.decode(encoded_string_to_string[string])) #because terminal doesn't usually allow bytes
     @rpc
     def square_odd(self, num_list):
         return [x*x if x%2==1 else x for x in num_list]
+
+
+#a case for each function
+a=Microservice()
+print(a.encode_strings(["Daniel","Is","Cool", "is" , "Is"]))
+print(a.square_odd([1,2,3,4,5,6,7,8,9,10]))
+print(a.decode_string("b'\\xfa _\\xa4'"))
 
 
 
